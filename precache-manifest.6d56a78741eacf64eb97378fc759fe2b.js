80d5c6f99907853510555e7ff0b36538de551ee2
KHilse <khilsedev@gmail.com>
2019-09-26 14:47:04 -0700

    Updates

---
+++ precache-manifest.6d56a78741eacf64eb97378fc759fe2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f32147f346827ce2fff00a156d7afe",
+    "url": "/portfolio-khilse/index.html"
+  },
+  {
+    "revision": "8dfbf95c438d8aa69e10",
+    "url": "/portfolio-khilse/static/css/main.4e0f9518.chunk.css"
+  },
+  {
+    "revision": "07776261b2e13b1d09f0",
+    "url": "/portfolio-khilse/static/js/2.0d5725fb.chunk.js"
+  },
+  {
+    "revision": "8dfbf95c438d8aa69e10",
+    "url": "/portfolio-khilse/static/js/main.97cf7b06.chunk.js"
+  },
+  {
+    "revision": "69b3b081a435b3702aa9",
+    "url": "/portfolio-khilse/static/js/runtime~main.b698ae4f.js"
+  }
+]);
