b39cc0196124ea35f998316e7446316312960bf7
Kirk Hilse <khilse@hotmail.com>
2019-09-25 21:18:51 -0700

    CSS Improvements

--- src/pages/projectitem.jsx
+++ src/pages/projectitem.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const ProjectItem = props => {
+    let translation = {transform: `translate(-50%, 30%) rotate(${props.angle}deg)`};
     return (
-        <div className="project-item">
+        <div className="project-item" style={translation}>
             <p><a href={props.url}>{props.title}</a></p>
             <img className="project-screenshot" src={props.screenshot} />
         </div>
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,30 +3,45 @@ import ProjectItem from './projectitem';
 
 class Projects extends React.Component {
 
-	constructor (props) {
-		super (props);
+	state = {
+		projects: [
+			["Asteroid Defense", "https://khilse.github.io/asteroid-defense/index.html", "./asteroid-defense.png", 180],
+			["Pedals and Pints", "https://pedals-and-pints-v1.herokuapp.com/", "./pedals-and-pints.png", 210]
+		]
+	}
 
-		this.state = {
-			projects: [
-				["Asteroid Defense", "https://khilse.github.io/asteroid-defense/index.html", "./asteroid-defense.png"],
-				["Pedals and Pints", "https://pedals-and-pints-v1.herokuapp.com/", "./pedals-and-pints.png"]
-			]
-		}
+	componentDidMount() {
+		console.log("mounted")
+		window.addEventListener('resize', this.updateDimensions);
+		setTimeout(this.handleTimeout.bind(this), 1000);
+	}
+
+	handleTimeout() {
+		console.log('timeout')
+		let state = JSON.parse(JSON.stringify(this.state));
+		console.log('STATE', state)
+		state.projects.forEach(project => {
+			project[3] -= 180;
+		})
+		this.setState(state);
+	}
+
+	updateDimensions() {
+		console.log('window dimensions changed')
+		// TODO: scale projectitems accordingly
 	}
 
 	render() {
-
 		let projects = this.state.projects.map((p,i) => {
-			let [title, url, screenshot] = p;
+			let [title, url, screenshot, angle] = p;
 
 			return (
-				<ProjectItem title={title} url={url} screenshot={screenshot} />
+				<ProjectItem key={i} title={title} url={url} screenshot={screenshot} angle={angle} />
 			)
 		})
 
-
 		return (
-			<div>
+			<div className="project-radius">
 				{projects}
 			</div>
 		)
