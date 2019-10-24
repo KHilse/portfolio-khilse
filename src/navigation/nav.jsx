2ccb3034f78c949fb6ddb9ca4523b7e1569dfbd1
KHilse <khilsedev@gmail.com>
2019-10-24 13:24:17 -0700

    Fixed navbar styling and mouse pointer type

--- src/navigation/nav.jsx
+++ src/navigation/nav.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 
 const Nav = props => {
 	return (
-		<nav className="nav">
-		  <ul onClick={props.handleNavClick}>
+		<nav className="nav-container">
+		  <ul className="nav-bar" onClick={props.handleNavClick}>
 			<li id="left" className="nav-item">&lt;</li>
 			<li id="right" className="nav-item">&gt;</li>
 	      </ul>
